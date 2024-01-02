5e897b3f931db97cf1b7ae937e13fb2b4ae008c7
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-01-02 12:59:08 +0900

    success_message実装.1.2.2024

--- kakomon/views.py
+++ kakomon/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import TemplateView, ListView, CreateView
 from django.urls import reverse_lazy
 from .constants import EXAMTYPE, NAVIGATION_OR_MECHANISM, GRADE, SUBJECT
@@ -104,8 +105,8 @@ class QuestionListView(ListView):
         return context
 
 
-"""試験登録"""
-class ExamCreateView(CreateView):
+"""定期試験登録"""
+class ExamCreateView(SuccessMessageMixin, CreateView):
     model = Exam
     form_class = ExamForm
     template_name = 'kakomon/create_exam.html'
@@ -113,10 +114,13 @@ class ExamCreateView(CreateView):
 
     def test_func(self):
         return self.request.user.is_contribute
-
+    
+    def get_success_message(self, cleaned_data):
+        self.success_message = f"定期試験 「{self.object.date.year}年 {self.object.date.month}月 {self.object.get_exam_type_display()} {self.object.get_grade_display()} {self.object.get_navigation_or_mechanism_display()}」 を登録しました。"
+        return self.success_message
 
 """科目登録"""
-class SubjectCreateView(CreateView):
+class SubjectCreateView(SuccessMessageMixin, CreateView):
     model = Subject
     form_class = SubjectForm
     template_name = 'kakomon/create_subject.html'
@@ -124,10 +128,14 @@ class SubjectCreateView(CreateView):
 
     def test_func(self):
         return self.request.user.is_contribute
+    
+    def get_success_message(self, cleaned_data):
+        self.success_message = f"科目「{self.object.get_name_display()}」 定期試験 「{self.object.exam.date.year}年 {self.object.exam.date.month}月 {self.object.exam.get_exam_type_display()} {self.object.exam.get_grade_display()} {self.object.exam.get_navigation_or_mechanism_display()}」 を登録しました。"
+        return self.success_message
 
 
-"""質問登録"""
-class QuestionCreateView(CreateView):
+"""問題登録"""
+class QuestionCreateView(SuccessMessageMixin, CreateView):
     model = Question
     form_class = QuestionForm
     template_name = 'kakomon/create_question.html'
@@ -135,3 +143,10 @@ class QuestionCreateView(CreateView):
 
     def test_func(self):
         return self.request.user.is_contribute
+    
+    def get_success_message(self, cleaned_data):
+        if self.object.edamon:
+            self.success_message = f"「大問{self.object.daimon}」「小問{self.object.shomon}」「枝問{self.object.edamon}」 定期試験「{self.object.subject.exam.date.year}年 {self.object.subject.exam.date.month}月 {self.object.subject.exam.get_exam_type_display()} {self.object.subject.exam.get_grade_display()} {self.object.subject.exam.get_navigation_or_mechanism_display()}」 科目「{self.object.subject.get_name_display()}」を登録しました。"
+        else:
+            self.success_message = f"「大問{self.object.daimon}」「小問{self.object.shomon}」 定期試験「{self.object.subject.exam.date.year}年 {self.object.subject.exam.date.month}月 {self.object.subject.exam.get_exam_type_display()} {self.object.subject.exam.get_grade_display()} {self.object.subject.exam.get_navigation_or_mechanism_display()}」 科目「{self.object.subject.get_name_display()}」 を登録しました。"
+        return self.success_message
