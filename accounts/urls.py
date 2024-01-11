3e8713a5363d44d91090cab4b1d67eddb0781f73
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-01-11 10:23:18 +0900

    アカウント作成画面.1.11.2024

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("signup/", views.SignupView.as_view(), name="signup"),
     path("profile/<int:pk>", views.ProfileView.as_view(), name="profile"),
     path("profile_change/<int:pk>", views.UserChangeView.as_view(), name="profile_change"),
 ]
