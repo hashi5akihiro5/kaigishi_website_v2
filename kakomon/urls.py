4b93b9d5140152001dff3840b0b3765b151fc0cf
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-02-09 10:51:47 +0900

    urlパス変更_02.09.2024

--- kakomon/urls.py
+++ kakomon/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'kakomon'
 
 urlpatterns = [
-    path('index', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('<exam_type>/<navigation_or_mechanism>/<grade>', views.ExamListView.as_view(), name='list_exam'),
     path('<exam_type>/<navigation_or_mechanism>/<grade>/<int:year>/<int:month>', views.SubjectListView.as_view(), name='list_subject'),
     path('<exam_type>/<navigation_or_mechanism>/<grade>/<int:year>/<int:month>/<subject>', views.QuestionListView.as_view(), name='list_question'),
