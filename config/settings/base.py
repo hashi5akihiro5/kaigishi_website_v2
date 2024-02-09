2bba6c35240a6e177057a645c22b1b286b560d37
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-02-09 10:59:48 +0900

    STATIC_ROOT変更_02.09.2024

--- config/settings/base.py
+++ config/settings/base.py
@@ -112,7 +112,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Media
 MEDIA_URL = 'media/'
