d9aabd3a89d9499360aaeb7210c334a0988781ef
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-02-15 11:58:26 +0900

    heroku settings

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -10,7 +10,7 @@ if not DEBUG:
     import django_heroku
     import dj_database_url
 
-    django_heroku.settigs(locals())
+    django_heroku.settings(locals())
 
     db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
     DATABASES['default'].update(db_from_env)
