3e84550fdcbbaed8b4744827c640e1ed5a6ca019
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-02-15 22:17:16 +0900

    メール設定

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,7 +136,14 @@ LOGIN_REDIRECT_URL = "kakomon:index" # 追加
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # お問合せ送信先
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SENDGRID_API_KEY= os.getenv('SENDGRID_API_KEY')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 
 # AWS設定
