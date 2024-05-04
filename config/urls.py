931c94c3fd3858cfb93730f6c246d65cefb44d21
akihiro.hashi <hashi5akihiro5@gmail.com>
2024-05-04 16:26:52 +0900

    機関名称変更 engineeringへ修正

--- config/urls.py
+++ config/urls.py
@@ -3,9 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from accounts import views
-
-handler500 = views.my_customized_server_error
+# from accounts import views
+# handler500 = views.my_customized_server_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
